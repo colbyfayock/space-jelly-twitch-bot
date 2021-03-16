77b7850199a0caa88ef120d26337e4d70b688d81
colbyfayock <fayock@gmail.com>
2021-03-15 21:53:58 -0400

    adding express to server to listen on heroku port

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 
+const express = require('express');
 const fetch = require('node-fetch');
 
 const { getClient } = require('./lib/twitch');
@@ -7,6 +8,23 @@ const { getCommandFromMessage, execCommand } = require('./lib/command');
 const User = require('./models/user');
 
 const prefix = `[${process.env.TWITCH_BOT_USERNAME}]`;
+
+/**
+ * App Client
+ */
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  const datetime = new Date().toISOString();
+  console.log(`${prefix} - ${datetime} - Listening on ${port}`);
+});
+
+/**
+ * Twitch Client
+ */
 
 const client = getClient();
 
