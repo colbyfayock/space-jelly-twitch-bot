09c07f2d226ab73b67b20e2ecdc6273810dc1144
colbyfayock <fayock@gmail.com>
2021-03-16 20:11:58 -0400

    removing express from worker

--- server/Procfile
+++ server/Procfile
@@ -1,1 +1,1 @@
-worker: node server.js
+worker: npm start
--- server/server.js
+++ server/server.js
@@ -1,26 +1,10 @@
 require('dotenv').config();
 
-const express = require('express');
-const fetch = require('node-fetch');
-
 const { getClient } = require('./lib/twitch');
 const { getCommandFromMessage, execCommand } = require('./lib/command');
 const User = require('./models/user');
 
 const prefix = `[${process.env.TWITCH_BOT_USERNAME}]`;
-
-/**
- * App Client
- */
-
-const app = express();
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-  const datetime = new Date().toISOString();
-  console.log(`${prefix} - ${datetime} - Listening on ${port}`);
-});
 
 /**
  * Twitch Client
