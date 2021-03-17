99c3e269efe6be21655b05e5e5fe0426a1c61e58
colbyfayock <fayock@gmail.com>
2021-03-16 20:59:14 -0400

    adding datetime eto logs

--- server/server.js
+++ server/server.js
@@ -38,55 +38,68 @@ client.on('message', async (channel, context, message) => {
 });
 
 client.on('subscription', (channel, userName, methods, message, tags) => {
+  const datetime = new Date().toISOString();
   console.log(`${prefix} - ${datetime} - subscription - ${userName} = ${methods} - ${message}`);
   client.say(channel, `Thanks ${userName} for the sub!`);
 });
 
 client.on('join', (channel, nickName) => {
+  const datetime = new Date().toISOString();
   console.log(`${prefix} - ${datetime} - join - ${nickName}`);
   client.say(channel, `Welcome ${nickName}!`);
 });
 
 client.on('cheer', (channel, tags, message) => {
+  const datetime = new Date().toISOString();
   console.log(`${prefix} - ${datetime} - cheer - ${message}`);
 });
 
 client.on('redeem', (channel, userName, type, tags, message) => {
+  const datetime = new Date().toISOString();
   console.log(`${prefix} - ${datetime} - redeem - ${userName} - ${type} - ${message}`);
 });
 
 client.on('subgift', (channel, userName, streakMonths, recipient, methods, tags) => {
+  const datetime = new Date().toISOString();
   console.log(`${prefix} - ${datetime} - subgift - ${userName} - ${streakMonths} - ${recipient} - ${methods}`);
 });
 
 client.on('anonsubgift', (channel, streakMonths, recipient, methods, tags) => {
+  const datetime = new Date().toISOString();
   console.log(`${prefix} - ${datetime} - anonsubgift - ${streakMonths} - ${recipient} - ${methods}`);
 });
 
 client.on('submysterygift', (channel, userName, giftSubCount, methods, tags) => {
+  const datetime = new Date().toISOString();
   console.log(`${prefix} - ${datetime} - submysterygift - ${userName} - ${giftSubCount} - ${methods}`);
 });
 
 client.on('anonsubmysterygift', (channel, giftSubCount, methods, tags) => {
+  const datetime = new Date().toISOString();
   console.log(`${prefix} - ${datetime} - anonsubmysterygift - ${giftSubCount} - ${methods}`);
 });
 
 client.on('primepaidupgrade', (channel, userName, methods, tags) => {
+  const datetime = new Date().toISOString();
   console.log(`${prefix} - ${datetime} - primepaidupgrade - ${userName} - ${methods}`);
 });
 
 client.on('giftpaidupgrade', (channel, userName, sender, tags) => {
+  const datetime = new Date().toISOString();
   console.log(`${prefix} - ${datetime} - giftpaidupgrade - ${userName} - ${sender}`);
 });
 
 client.on('anongiftpaidupgrade', (channel, userName, tags) => {
+  const datetime = new Date().toISOString();
   console.log(`${prefix} - ${datetime} - anongiftpaidupgrade - ${userName}`);
 });
 
 client.on('raid', (channel, userName, viewers, tags) => {
+  const datetime = new Date().toISOString();
   console.log(`${prefix} - ${datetime} - raid - ${userName} - ${viewers}`);
 });
 
 client.on('ritual', (ritualName, channel, userName, tags, message) => {
+  const datetime = new Date().toISOString();
   console.log(`${prefix} - ${datetime} - ritual - ${ritualName}`);
 });
